a7bc3c5ed49c3a4cedb902b9e79e12f0e90230ef
Louise Benson <louise.benson@gmail.com>
2023-08-03 16:40:39 +0000

    Add admin panel to manage user posts

--- furniture/admin.py
+++ furniture/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Furniture, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Furniture)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
