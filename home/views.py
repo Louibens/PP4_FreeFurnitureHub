9a656b815f2bd324cb7c4764dd5d6f55dd96e74c
Louise Benson <louise.benson@gmail.com>
2023-08-04 16:33:02 +0000

    Set up base template and index home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from furniture.models import Furniture
 
 # Create your views here.
+
+
+class Index(generic.ListView):
+    model = Furniture
+    queryset = Furniture.objects.order_by('-posted_date')
+    template_name = 'index.html'
+    paginate = 6
