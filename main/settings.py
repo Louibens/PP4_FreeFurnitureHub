6be147e9f9f0248d2332e19ea3894dcc8331ad8b
Louise Benson <louise.benson@gmail.com>
2023-08-06 12:27:56 +0000

    Reverted debug changes to fix deploy

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-louibens-pp4freefurnitu-50sdv7hyyf9.ws-eu102.gitpod.io', 'free-furniture-hub-10db98e468e6.herokuapp.com', 'localhost']
 
