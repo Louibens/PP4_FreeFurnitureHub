9246bc56c4f8acacd5c696d42b5fbb8af5322979
Louise Benson <louise.benson@gmail.com>
2023-08-06 11:53:00 +0000

    Add How It Works view and content

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Index
+from .views import Index, HowItWorks
 
 
 urlpatterns = [
     path('', Index.as_view(), name='home'),
+    path('howitworks/', HowItWorks.as_view(), name='howitworks'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic, View
+from django.views.generic import ListView
 from furniture.models import Furniture
 
 # Create your views here.
 
 
-class Index(generic.ListView):
+class Index(ListView):
     model = Furniture
     queryset = Furniture.objects.order_by('-posted_date')
     template_name = 'home/index.html'
-    paginate = 6
     context_object_name = 'furniture_items'
 
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
+
+
+class HowItWorks(ListView):
+    template_name = 'home/how_it_works.html'
+    model = Furniture
+ 
     def get_queryset(self):
         return self.model.objects.all()[:3]
