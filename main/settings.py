90ecf44669242e6e3cd9e541433fd174fe41d161
Louise Benson <louise.benson@gmail.com>
2023-08-11 15:07:30 +0000

    Fix active class applying to allauth form pages and reduce margins on furniture cards

--- main/settings.py
+++ main/settings.py
@@ -33,7 +33,7 @@ DEBUG = 'DEBUG' in os.environ
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['8000-louibens-pp4freefurnitu-50sdv7hyyf9.ws-eu102.gitpod.io', 'free-furniture-hub-10db98e468e6.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['8000-louibens-pp4freefurnitu-50sdv7hyyf9.ws-eu103.gitpod.io', 'free-furniture-hub-10db98e468e6.herokuapp.com', 'localhost']
 
 # Application definition
 
